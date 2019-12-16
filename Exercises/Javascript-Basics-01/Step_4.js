d8d288b433ecd86bc4574f0d831e0c167b8eef83
ghadban135 <ahmad135886@gmail.com>
2019-12-16 14:46:54 +0200

    completed step_4.js

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("what is your name");
+var surname = prompt("what is your surname");
+var city = prompt("what is your city");
+alert("hello " + name + " " + surname + " from " + city);
