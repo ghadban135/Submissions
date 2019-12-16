9d43c8c645155de23e688cb18519693706d04a32
ghadban135 <ahmad135886@gmail.com>
2019-12-16 15:23:25 +0200

    completed step_5.js

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function mul() {
+  var val1 = document.getElementById("first_number").value;
+  var val2 = document.getElementById("second_number").value;
+  var val3 = val1 * val2;
+  alert(val1 + "*" + val2 + "=" + val3);
+}
