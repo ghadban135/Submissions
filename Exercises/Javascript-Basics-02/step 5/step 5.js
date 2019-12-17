1d19919045c8ee696d65055b28a5c0f80e404170
ghadban135 <ahmad135886@gmail.com>
2019-12-17 14:59:41 +0200

    completed JS basics-2 step 5

---
+++ Exercises/Javascript-Basics-02/step 5/step 5.js
@@ -0,0 +1,18 @@
+function change() {
+  image1.addEventListener("mouseover", function() {
+    this.src = "images/image1_2.jpg";
+  });
+  image2.addEventListener("mouseover", function() {
+    this.src = "images/image2_2.jpg";
+  });
+  image3.addEventListener("mouseover", function() {
+    this.src = "images/image3_2.jpg";
+  });
+  image4.addEventListener("mouseover", function() {
+    this.src = "images/image4_2.jpg";
+  });
+  image5.addEventListener("mouseover", function() {
+    this.src = "images/image5_2.jpg";
+  });
+}
+change();
