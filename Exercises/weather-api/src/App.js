1251f4dd725f6f691f16c8c1f40bd1f9ff94f018
ghadban135 <ahmad135886@gmail.com>
2020-01-15 13:11:14 +0200

    Weather API: Extracting the values from the Fake Weather json

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -5,6 +5,7 @@ import clear from "./img/weather-icons/clear.svg";
 import Search from "./search";
 import CurrentWeather from "./currentWeather";
 import DuringDay from "./duringDay";
+import FakeWeather from "./data/FakeWeather.json";
 
 class App extends Component {
   constructor(props) {
@@ -18,58 +19,58 @@ class App extends Component {
         <Search></Search>
 
         <CurrentWeather
-          img={mostlycloudy}
-          alt="mostlycloudy"
-          desWeather="overcast clouds"
-          avgMin="10"
-          avgMax="11"
-          Humidity="78"
-          Pressure="1008.48"
+          img={clear}
+          alt={FakeWeather.list[29].weather[0].description}
+          desWeather={FakeWeather.list[29].weather[0].description}
+          avgMin={FakeWeather.list[29].main.temp_min}
+          avgMax={FakeWeather.list[29].main.temp_max}
+          Humidity={FakeWeather.list[29].main.humidity}
+          Pressure={FakeWeather.list[29].main.pressure}
         ></CurrentWeather>
 
         <div className="footer">
           <div className="small"></div>
           <DuringDay
-            time="03:00"
+            time={FakeWeather.list[29].dt_txt}
+            smallImg={clear}
+            smallAlt={FakeWeather.list[29].weather[0].description}
+            temp={FakeWeather.list[29].main.temp}
+          ></DuringDay>
+          <DuringDay
+            time={FakeWeather.list[30].dt_txt}
+            smallImg={clear}
+            smallAlt={FakeWeather.list[30].weather[0].description}
+            temp={FakeWeather.list[30].main.temp}
+          ></DuringDay>
+          <DuringDay
+            time={FakeWeather.list[31].dt_txt}
+            smallImg={clear}
+            smallAlt={FakeWeather.list[31].weather[0].description}
+            temp={FakeWeather.list[31].main.temp}
+          ></DuringDay>
+          <DuringDay
+            time={FakeWeather.list[32].dt_txt}
+            smallImg={clear}
+            smallAlt={FakeWeather.list[32].weather[0].description}
+            temp={FakeWeather.list[32].main.temp}
+          ></DuringDay>
+          <DuringDay
+            time={FakeWeather.list[33].dt_txt}
+            smallImg={clear}
+            smallAlt={FakeWeather.list[33].weather[0].description}
+            temp={FakeWeather.list[33].main.temp}
+          ></DuringDay>
+          <DuringDay
+            time={FakeWeather.list[34].dt_txt}
             smallImg={mostlycloudy}
-            smallAlt="mostlycloudy"
-            temp="8"
+            smallAlt={FakeWeather.list[34].weather[0].description}
+            temp={FakeWeather.list[34].main.temp}
           ></DuringDay>
           <DuringDay
-            time="06:00"
+            time={FakeWeather.list[35].dt_txt}
             smallImg={mostlycloudy}
-            smallAlt="mostlycloudy"
-            temp="9"
-          ></DuringDay>
-          <DuringDay
-            time="09:00"
-            smallImg={clear}
-            smallAlt="clear"
-            temp="14"
-          ></DuringDay>
-          <DuringDay
-            time="12:00"
-            smallImg={clear}
-            smallAlt="clear"
-            temp="17"
-          ></DuringDay>
-          <DuringDay
-            time="15:00"
-            smallImg={clear}
-            smallAlt="clear"
-            temp="18"
-          ></DuringDay>
-          <DuringDay
-            time="18:00"
-            smallImg={clear}
-            smallAlt="clear"
-            temp="16"
-          ></DuringDay>
-          <DuringDay
-            time="21:00"
-            smallImg={mostlycloudy}
-            smallAlt="mostlycloudy"
-            temp="13"
+            smallAlt={FakeWeather.list[35].weather[0].description}
+            temp={FakeWeather.list[35].main.temp}
           ></DuringDay>
           <div className="small"></div>
         </div>
