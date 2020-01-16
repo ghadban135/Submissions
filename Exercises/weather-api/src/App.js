3670f115fc190d212c32b41718472c8b7cdeb00e
ghadban135 <ahmad135886@gmail.com>
2020-01-16 13:15:47 +0200

    finish api weather exercices

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import mostlyCloudy from "./img/weather-icons/mostlycloudy.svg";
 import clear from "./img/weather-icons/clear.svg";
 import drizzle from "./img/weather-icons/drizzle.svg";
 import rain from "./img/weather-icons/rain.svg";
 import snow from "./img/weather-icons/snow.svg";
 import fog from "./img/weather-icons/fog.svg";
-import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import partlyCloudy from "./img/weather-icons/partlycloudy.svg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import storm from "./img/weather-icons/storm.svg";
 import Search from "./search";
 import CurrentWeather from "./currentWeather";
 import DuringDay from "./duringDay";
 import FakeWeather from "./data/FakeWeather.json";
+
+
+function getImg(id) {
+  let val = "";
+  if (id < 300) val = storm;
+  else if (id >= 300 && id < 500) val = drizzle;
+  else if (id >= 500 && id < 599) val = rain;
+  else if (id >= 600 && id < 699) val = snow;
+  else if (id >= 700 && id < 799) val = fog;
+  else if (id === 800) val = clear;
+  else if (id === 801) val = partlyCloudy;
+  else if (id === 802) val = cloudy;
+  else if (id > 801 && id < 806) val = mostlyCloudy;
+  return val;
+}
 
 class App extends Component {
   constructor(props) {
@@ -26,6 +43,7 @@ class App extends Component {
       
     };
   }
+  
  
  getWeather = async (e) =>{
    e.preventDefault();
@@ -57,6 +75,13 @@ class App extends Component {
       time5:(data.list[4].dt_txt).slice(11,16),
       time6:(data.list[5].dt_txt).slice(11,16),
       time7:(data.list[6].dt_txt).slice(11,16),
+      img1:getImg(data.list[0].weather[0].id),
+      img2:getImg(data.list[1].weather[0].id),
+      img3:getImg(data.list[2].weather[0].id),
+      img4:getImg(data.list[3].weather[0].id),
+      img5:getImg(data.list[4].weather[0].id),
+      img6:getImg(data.list[5].weather[0].id),
+      img7:getImg(data.list[6].weather[0].id),
       
       error:""  
     }); 
@@ -74,7 +99,7 @@ class App extends Component {
         <Search getWeather={this.getWeather}></Search>
 
         <CurrentWeather
-          img={clear}
+          img={this.state.img1}
           alt={this.state.desWeather}
           desWeather={this.state.desWeather}
           avgMin={this.state.avgMin}
@@ -87,37 +112,37 @@ class App extends Component {
           <div className="small"></div>
           <DuringDay
             time={this.state.time1}
-            smallImg={clear}
+            smallImg={this.state.img1}
             temp={this.state.temp1}
           ></DuringDay>
           <DuringDay
             time={this.state.time2}
-            smallImg={clear}
+            smallImg={this.state.img2}
             temp={this.state.temp2}
           ></DuringDay>
           <DuringDay
             time={this.state.time3}
-            smallImg={clear}
+            smallImg={this.state.img3}
             temp={this.state.temp3}
           ></DuringDay>
           <DuringDay
             time={this.state.time4}
-            smallImg={clear}
+            smallImg={this.state.img4}
             temp={this.state.temp4}
           ></DuringDay>
           <DuringDay
             time={this.state.time5}
-            smallImg={clear}
+            smallImg={this.state.img5}
             temp={this.state.temp5}
           ></DuringDay>
           <DuringDay
             time={this.state.time6}
-            smallImg={mostlycloudy}
+            smallImg={this.state.img6}
             temp={this.state.temp6}
           ></DuringDay>
           <DuringDay
             time={this.state.time7}
-            smallImg={mostlycloudy}
+            smallImg={this.state.img7}
             temp={this.state.temp7}
           ></DuringDay>
           <div className="small"></div>
