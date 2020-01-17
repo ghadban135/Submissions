dbbd260c9942cdc7dcd8a57b8740abc211fb8e80
ghadban135 <ahmad135886@gmail.com>
2020-01-17 16:46:01 +0200

    Creating Table

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -6,3 +6,26 @@ select points from students where Name='Alex'
 insert into students(Name,gender,age,Points) values('Malako','F',21,900)
 update students set points=points+50 where Name='Basma'
 update students set points=points-5 where Name='Alex'
+
+#create table
+
+    create table graduates(
+    ID INTEGER NOT NULL PRIMARY KEY autoincrement,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation date
+    );
+
+INSERT INTO graduates('Id','Name','Age','Gender','Points')
+SELECT * FROM students
+WHERE Name='Layal' 
+
+
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE Name='Layal' ;
+
+
+DELETE FROM students WHERE Name='Layal';
