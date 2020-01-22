e1ac0f4e6918fcf4583bdaf3e25b1a3bf1836c3e
ghadban135 <ahmad135886@gmail.com>
2020-01-22 12:33:13 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -11,6 +11,16 @@ const PORT = 8080;
 app.get("/", (req, res) => {
   res.send("ok");
 });
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  var d = new Date();
+  var TIME1 = d.getHours();
+  var TIME2 = d.getMinutes();
+  res.send({ status: 200, message: `${TIME1}:${TIME2}` });
+});
 
 // make the server listen to requests
 app.listen(PORT, () => {
